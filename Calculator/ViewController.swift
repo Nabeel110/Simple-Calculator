92be11ba044856ba37df808e22c2d3a6d61ed7f2
Nabeel Ahmed <nabeel.ahmed@sadapay.pk>
2023-02-23 10:27:30 +0500

    Completed Simple Calculator in swift

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    var resultNumber  = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func add(_ sender: UIButton) {
+        if let firstNumber  =  Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                resultNumber =  firstNumber + secondNumber
+                result.text = String(resultNumber)
+            }
+        }
+    }
+    
+    @IBAction func subtract(_ sender: UIButton) {
+        if let firstNumber  =  Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                resultNumber =  firstNumber - secondNumber
+                result.text = String(resultNumber)
+            }
+        }
+    }
+    
+    @IBAction func mutiply(_ sender: UIButton) {
+        if let firstNumber  =  Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                resultNumber =  firstNumber * secondNumber
+                result.text = String(resultNumber)
+            }
+        }
+    }
+    
+    @IBAction func divide(_ sender: UIButton) {
+        if let firstNumber  =  Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                resultNumber =  firstNumber / secondNumber
+                result.text = String(resultNumber)
+            }
+        }
+    }
 }
 
